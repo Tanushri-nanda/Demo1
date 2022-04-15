12a82a0ff3db2600da71f78f0070fbf7bce34921
Tanushri-nanda <103626267+Tanushri-nanda@users.noreply.github.com>
2022-04-15 10:57:59 +0530

    Add files via upload
    commit2

---
+++ Demo.git/src/main/java/com/demo/git/Application.java
@@ -0,0 +1,16 @@
+package com.demo.git;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+		System.out.println("Hello");
+		System.out.println("Demo");
+		
+	}
+
+}
